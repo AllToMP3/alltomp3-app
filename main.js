ca51be6d81b14e610ccb38e94ff087faece1e8a1
Basile Bruneau <basilebruneau@gmail.com>
2018-02-24 19:03:58 +0100

    Remove production conf

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const ncp = require('ncp');
 const request = require('request');
 const alltomp3 = require('alltomp3');
 const VERSION = app.getVersion();
-const DEV = false;
+const DEV = true;
 
 // autoUpdater
 if (!DEV && os.platform() != 'linux') {
