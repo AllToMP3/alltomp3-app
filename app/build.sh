a47dfce2e6709831d5cda399f18f17aaaeb33a2a
Basile Bruneau <basilebruneau@gmail.com>
2018-11-18 22:59:38 +0100

    Build German and Arabic translations

--- app/build.sh
+++ app/build.sh
@@ -7,3 +7,5 @@ ng build --aot --output-path dist/tr --locale tr --i18n-format xlf --i18n-file s
 ng build --aot --output-path dist/ja --locale ja --i18n-format xlf --i18n-file src/locale/messages.ja.xlf --base-href './'
 ng build --aot --output-path dist/es --locale es --i18n-format xlf --i18n-file src/locale/messages.es.xlf --base-href './'
 ng build --aot --output-path dist/pt --locale pt --i18n-format xlf --i18n-file src/locale/messages.pt.xlf --base-href './'
+ng build --aot --output-path dist/de --locale de --i18n-format xlf --i18n-file src/locale/messages.de.xlf --base-href './'
+ng build --aot --output-path dist/ar --locale ar --i18n-format xlf --i18n-file src/locale/messages.ar.xlf --base-href './'
